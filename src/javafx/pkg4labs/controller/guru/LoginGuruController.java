a710634933bd708d5d645f722bf959c97f62ab07
syihab <syihabudin234@gmail.com>
2020-04-04 13:26:37 +0700

    Login selesai, register dalam perbaikan

--- src/javafx/pkg4labs/controller/guru/LoginGuruController.java
+++ src/javafx/pkg4labs/controller/guru/LoginGuruController.java
@@ -5,18 +5,26 @@
  */
 package javafx.pkg4labs.controller.guru;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.pkg4labs.controller.siswa.MyConnection;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -24,15 +32,23 @@ import javafx.stage.Stage;
  * @author Muhammad Fahru Rozi
  */
 public class LoginGuruController implements Initializable {
-
+    Connection koneksi;
+    Statement stmt = null;
+    ResultSet res = null;
+    
+    
     @FXML
     private TextField username;
-
+    
+    @FXML 
+    private PasswordField password;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        koneksi = MyConnection.getKoneksi("localhost", "3306", "root", "", "project_java");
         // TODO
     }    
 
@@ -51,6 +67,68 @@ public class LoginGuruController implements Initializable {
         
         Stage stage = (Stage) node.getScene().getWindow();        
         stage.setScene(new Scene(root));
+    }
+    
+    @FXML
+    private void login(javafx.scene.input.MouseEvent event) throws IOException {
+         try{
+             
+            String user = username.getText();
+            String pass = password.getText();
+            String real_pass;
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+
+            digest.reset();
+
+            digest.update(pass.getBytes("utf8"));
+
+            real_pass = String.format("%040x", new BigInteger(1, digest.digest()));
+
+            
+
+            String sql = "SELECT * FROM teachers WHERE username = '"+ user + "' AND password = '"+real_pass+"'";
+
+            stmt = (Statement) koneksi.createStatement();
+
+            res = stmt.executeQuery(sql);
+
+            
+
+            if(res.next()){
+
+                if(user.equals(res.getString("username")) && real_pass.equals(res.getString("password"))){
+
+                     JOptionPane.showMessageDialog(null, "Login Berhasil");
+
+                    System.out.println("Berhasil");
+                    Parent root = FXMLLoader.load(getClass().getResource("/javafx/pkg4labs/view/HalamanUtama.fxml"));
+                    Node node = (Node) event.getSource();
+
+                    Stage stage = (Stage) node.getScene().getWindow();        
+                    stage.setScene(new Scene(root));
+
+                } 
+
+            }
+
+            else{
+
+                    JOptionPane.showMessageDialog(null, "Login Gagal");
+                    System.out.println("Gagal");
+
+            }
+
+        }
+
+        catch(Exception Ex){
+
+             JOptionPane.showMessageDialog(null, "Terjadi Kesalahan pada Database");
+
+             System.out.println(Ex);
+
+        } 
+        
     }
     
 }
--- src/javafx/pkg4labs/controller/siswa/LoginSiswaController.java
+++ src/javafx/pkg4labs/controller/siswa/LoginSiswaController.java
@@ -4,9 +4,13 @@
  * and open the template in the editor.
  */
 package javafx.pkg4labs.controller.siswa;
-
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,9 +18,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -25,14 +31,25 @@ import javafx.stage.Stage;
  */
 public class LoginSiswaController implements Initializable {
 
+    Connection koneksi;
+    Statement stmt = null;
+    ResultSet res = null;
+    
+    
     @FXML
     private TextField username;
+    
+    @FXML 
+    private PasswordField password;
+    
+    
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+         koneksi = MyConnection.getKoneksi("localhost", "3306", "root", "", "project_java");
         // TODO
     }    
 
@@ -54,10 +71,63 @@ public class LoginSiswaController implements Initializable {
 
     @FXML
     private void loginSuccess(javafx.scene.input.MouseEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/javafx/pkg4labs/view/siswa/HalamanUtama.fxml"));
-        Node node = (Node) event.getSource();        
-        Stage stage = (Stage) node.getScene().getWindow();        
-        stage.setScene(new Scene(root));
+         try{
+
+            String user = username.getText();
+            String pass = password.getText();
+            String real_pass;
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+
+            digest.reset();
+
+            digest.update(pass.getBytes("utf8"));
+
+            real_pass = String.format("%040x", new BigInteger(1, digest.digest()));
+
+            
+
+            String sql = "SELECT * FROM students WHERE username = '"+ user + "' AND password = '"+real_pass+"'";
+
+            stmt = (Statement) koneksi.createStatement();
+
+            res = stmt.executeQuery(sql);
+
+            
+
+            if(res.next()){
+
+                if(user.equals(res.getString("username")) && real_pass.equals(res.getString("password"))){
+
+                    JOptionPane.showMessageDialog(null, "Login Berhasil");
+
+                    System.out.println("Berhasil");
+                    Parent root = FXMLLoader.load(getClass().getResource("/javafx/pkg4labs/view/siswa/HalamanUtama.fxml"));
+                    Node node = (Node) event.getSource();        
+                    Stage stage = (Stage) node.getScene().getWindow();        
+                    stage.setScene(new Scene(root));
+
+                } 
+
+            }
+
+            else{
+
+                    JOptionPane.showMessageDialog(null, "Login Gagal");
+                    System.out.println("Gagal");
+
+            }
+
+        }
+
+        catch(Exception Ex){
+
+             JOptionPane.showMessageDialog(null, "Terjadi Kesalahan pada Database");
+
+             System.out.println(Ex);
+
+        } 
+        
     }
     
 }
