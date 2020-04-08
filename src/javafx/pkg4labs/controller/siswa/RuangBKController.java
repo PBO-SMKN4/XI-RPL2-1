ba14b58e70b208c81233fc6074bba8501eb73c16
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2020-04-09 00:38:45 +0700

    Tambahan Halaman Peringatan

--- src/javafx/pkg4labs/controller/siswa/RuangBKController.java
+++ src/javafx/pkg4labs/controller/siswa/RuangBKController.java
@@ -76,5 +76,14 @@ public class RuangBKController implements Initializable {
         Stage stage = (Stage) node.getScene().getWindow();        
         stage.setScene(new Scene(root));
     }
+
+    @FXML
+    private void peringatan(javafx.scene.input.MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/javafx/pkg4labs/view/siswa/Peringatan.fxml"));
+        Node node = (Node) event.getSource();
+        
+        Stage stage = (Stage) node.getScene().getWindow();        
+        stage.setScene(new Scene(root));
+    }
     
 }
