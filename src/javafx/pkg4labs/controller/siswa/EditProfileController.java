8abe4daeea0babfba7bccc8bef5d5526816db28c
Dean Beniqno A <dbpower20@gmail.com>
2020-04-12 23:35:23 +0700

    Backend Profile Siswa

--- src/javafx/pkg4labs/controller/siswa/EditProfileController.java
+++ src/javafx/pkg4labs/controller/siswa/EditProfileController.java
@@ -7,6 +7,12 @@ package javafx.pkg4labs.controller.siswa;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -16,8 +22,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -25,24 +36,168 @@ import javafx.stage.Stage;
  * @author Muhammad Fahru Rozi
  */
 public class EditProfileController implements Initializable {
-
-    @FXML
-    public ComboBox<String> combobox1;    
-    ObservableList<String> list1 = FXCollections.observableArrayList("XI RPL 1","XI RPL 2","XI RPL 3");
+    
+    Connection koneksi;
+    String nis;
+    boolean edit = false;
+    int i;
     
     @FXML
-    public ComboBox<String> combobox2;
-    ObservableList<String> list2 = FXCollections.observableArrayList("Laki - Laki","Perempuan","Rahasia");
+    public ComboBox<String> comb_kelas;    
+    ObservableList<String> kelas = FXCollections.observableArrayList();
     
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        combobox1.setItems(list1);
-        combobox2.setItems(list2);
-    }    
+    @FXML
+    public ComboBox<String> comb_kelamin;    
+    ObservableList<String> kelamin = FXCollections.observableArrayList();
+    
+    @FXML
+    private TextField inp_nis;
+    
+    @FXML
+    private TextField inp_nama;
+    
+    @FXML
+    private TextField inp_email;
+    
+    @FXML
+    private TextField inp_username;
+    
+    @FXML
+    private DatePicker inp_tanggal;
+    
+    @FXML
+    private PasswordField inp_password;
+    
+    @FXML
+    private Button butt_edit;
+    
+  public void initialize(URL url, ResourceBundle rb) {
+                
+        try{
+        
+            koneksi = MyConnection.getKoneksi("localhost", "3306", "root", "", "project_java");
+            Statement stmt = koneksi.createStatement();
+            
+            nis = SessionSiswa.getSession();
+            int length = 0;
+            
+            ObservableList<String> data = FXCollections.observableArrayList("Laki-laki","Perempuan");
+            comb_kelamin.setItems(data);
+            
+             String query = "SELECT count(classes.nama_kelas) AS length FROM classes";     
+             ResultSet rsItung = stmt.executeQuery(query);
+           
+             if(rsItung.first()){
+                   length = rsItung.getInt("length");
+              }   
+        
+             String[] listKelas = new String[length];
+        
+             String queryKelas = "SELECT * FROM classes";
+             ResultSet rsKelas = stmt.executeQuery(queryKelas);
+             
+             while(rsKelas.next()){
+                listKelas[i] = rsKelas.getString("nama_kelas");
+                i++;
+             }
+        
+             ObservableList<String> kelas = FXCollections.observableArrayList(listKelas);
+             comb_kelas.setItems(kelas);
+        
+          }catch(Exception e){
+                JOptionPane.showMessageDialog(null, e);
+                 e.printStackTrace();
+        }
+       showData();
+    }
+        
+    
+  
+     
+    public void edit(){
+        if(!edit){
+            setEditButton(true);
+            butt_edit.setText("Simpan");
+            edit = true;
+        }
+        else{
+            simpanEdit();
+            butt_edit.setText("Edit");
+            edit = false;
+            setEditButton(false);
+            
+        }
+    }
+    
+     public void setEditButton(Boolean control){
+        inp_nis.setEditable(control);
+        inp_nama.setEditable(control);
+        inp_tanggal.setEditable(control);
+        inp_email.setEditable(control);
+        inp_username.setEditable(control);
+        comb_kelas.setEditable(control);
+        comb_kelamin.setEditable(control);
+    }
+    
+     void showData() {
+        try{
+            Statement stmt = koneksi.createStatement();
+            String query = "SELECT * FROM students WHERE nis = '"+nis+"'";
+            ResultSet rs = stmt.executeQuery(query);
+            
+            if(rs.next()){
+                inp_nis.setText(rs.getString("nis"));
+                inp_nama.setText(rs.getString("nama"));
+                comb_kelas.setValue(rs.getString("nama_kelas"));
+                comb_kelamin.setValue(rs.getString("jk"));
+                inp_tanggal.setValue(LocalDate.parse(rs.getString("tgl_lahir")));
+                inp_username.setText(rs.getString("username"));
+                inp_email.setText(rs.getString("email"));
+            }
+            
+        } catch (SQLException ex){
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Terjadi kesalahan query");
+        }
+    }
+    
+    
+    
+    public void simpanEdit(){
+        
+        String nis          = inp_nis.getText();
+        String nama         = inp_nama.getText();
+        String nama_kelas   = comb_kelas.getValue();
+        String jk           = comb_kelamin.getValue();
+        LocalDate tgl_lahir = inp_tanggal.getValue();
+        String username     = inp_username.getText();
+        String email        = inp_email.getText();
+        
+         try{
+            Statement stmt = koneksi.createStatement();
+            String query = "UPDATE students SET nama = '"+nama+"',"
+                      + "nama_kelas  = '"+nama_kelas+"',"
+                      + "jk          = '"+jk+"',"
+                      + "tgl_lahir   = '"+tgl_lahir+"',"
+                      + "username    = '"+username+"',"
+                      + "email       = '"+email+"' WHERE nis = '"+nis+"'";
+            
+            System.out.println(query);
+            int berhasil = stmt.executeUpdate(query);
+            if (berhasil == 1){
+                JOptionPane.showMessageDialog(null, "Data Berhasil Diubah");
+                 System.out.println("Berhasil");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Data gagal diubah");
+                System.out.println("Gagal");
+            }
+            
+            } catch(SQLException ex){
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Terjadi kesalahan pada query");
+        }
+    }  
 
 @FXML
     private void ruangBK(javafx.scene.input.MouseEvent event) throws IOException {
@@ -60,7 +215,7 @@ public class EditProfileController implements Initializable {
         
         Stage stage = (Stage) node.getScene().getWindow();        
         stage.setScene(new Scene(root));
-    }
+    } 
 
     @FXML
     private void jadwalPelajaran(javafx.scene.input.MouseEvent event) throws IOException {
